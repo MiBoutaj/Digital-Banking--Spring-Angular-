00686b10b44622ac7238fac3ba8d1c33aef3ea9c
Amine Aboutaj <aboutaj.amine2001@gmail.com>
2022-05-04 03:35:34 +0000

    First commit via IntellijIDEA

---
+++ ebanking-backend/src/main/java/org/sid/ebankingbackend/entities/AccountOperation.java
@@ -0,0 +1,25 @@
+package org.sid.ebankingbackend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.ebankingbackend.enums.OperationType;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class AccountOperation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    private OperationType type;
+    @ManyToOne
+    private BankAccount bankAccount;
+}
