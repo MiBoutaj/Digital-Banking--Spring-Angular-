be431a19f461b0829095a3d81fc551e45eb993fb
Amine Aboutaj <aboutaj.amine2001@gmail.com>
2022-05-25 20:02:57 +0100

    Customer Service

--- digital-banking-web/src/app/accounts/accounts.component.ts
+++ digital-banking-web/src/app/accounts/accounts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {Observable} from "rxjs";
+import {AccountDetails} from "../model/account.model";
 
 @Component({
   selector: 'app-accounts',
@@ -6,6 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accounts.component.css']
 })
 export class AccountsComponent implements OnInit {
+
+  accountFromGroup! : FormGroup;
+  currentPage : number =0;
+  pageSize : number =5;
+  accountObservable! : Observable<AccountDetails>
+  operationFromGroup! : FormGroup;
+  errorMessage! :string ;
+
 
   constructor() { }
 
--- digital-banking-web/src/app/customers/customers.component.ts
+++ digital-banking-web/src/app/customers/customers.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {catchError, map, Observable, throwError} from "rxjs";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Customer} from "../model/customer.model";
+import {Router} from "@angular/router";
+import {CustomerService} from "../services/customer.service";
 
 @Component({
   selector: 'app-customers',
@@ -7,9 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
-  constructor() { }
+  customers! : Observable<Array<Customer>>;
+  errorMessage!: string;
+  searchFormGroup : FormGroup | undefined;
+
+  constructor(private customerService : CustomerService,
+              private fb : FormBuilder,
+              private router : Router) { }
 
   ngOnInit(): void {
+    this.searchFormGroup=this.fb.group({
+      keyword : this.fb.control("")
+    });
+    this.handleSearchCustomers();
   }
+
+  handleSearchCustomers() {
+    let kw=this.searchFormGroup?.value.keyword;
+    this.customers=this.customerService.searchCustomers(kw).pipe(
+      catchError(err => {
+        this.errorMessage=err.message;
+        return throwError(err);
+      })
+    );
+  }
+
+
+  handleDeleteCustomer(c: Customer) {
+    let conf = confirm("Are you sure?");
+    if(!conf) return;
+    this.customerService.deleteCustomer(c.id).subscribe({
+      next : (resp) => {
+        this.customers=this.customers.pipe(
+          map(data=>{
+            let index=data.indexOf(c);
+            data.slice(index,1)
+            return data;
+          })
+        );
+      },
+      error : err => {
+        console.log(err);
+      }
+    })
+  }
+
+  handleCustomerAccounts(customer: Customer) {
+    this.router.navigateByUrl("/customer-accounts/"+customer.id,{state :customer});
+  }
+
+
 
 }
