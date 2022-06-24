c8924d95c5fcc6c559221084f0efc5db3474e354
Amine Aboutaj <aboutaj.amine2001@gmail.com>
2022-06-24 14:54:49 +0100

    end security

--- Security/src/main/java/com/micro/security/SecurityApplication.java
+++ Security/src/main/java/com/micro/security/SecurityApplication.java
@@ -1,13 +1,48 @@
 package com.micro.security;
 
+import com.micro.security.entities.Role;
+import com.micro.security.entities.User;
+import com.micro.security.service.UserService;
+import com.micro.security.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
 
+
+    @Bean
+    BCryptPasswordEncoder getBCE() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    CommandLineRunner commandLineRunner(UserService userService) {
+        return args -> {
+            userService.addRole(new Role(null, "ADMIN"));
+            userService.addRole(new Role(null, "USER"));
+
+            //ajouter les users
+            userService.saveUser(new User(null, "admin", "123", true, null));
+            userService.saveUser(new User(null, "amine", "123", true, null));
+
+            //ajouter les rôles aux users
+            userService.addRoleToUser("admin", "ADMIN");
+            userService.addRoleToUser("admin", "USER");
+            userService.addRoleToUser("amine", "USER");
+
+
+        };
+    }
 }
---
+++ Security/src/main/java/com/micro/security/service/UserService.java
@@ -0,0 +1,16 @@
+package com.micro.security.service;
+
+import com.micro.security.entities.Role;
+import com.micro.security.entities.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+    User findUserByUsername (String username);
+    Role addRole(Role role);
+    User addRoleToUser(String username, String rolename);
+    List<User> findAllUsers();
+
+
+}
